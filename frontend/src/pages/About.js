32ac119f5e873b69adb4351752d5c7b3a960f2e6
melkzedek wafula <melkywafula29@gmail.comm>
2025-10-11 07:56:38 +0300

    changes

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -8,6 +8,7 @@ const About = () => {
     return (
         <div>
             <h1>About This Application</h1>
+            <p>This application is built using React and demonstrates basic routing and component structure.</p>
             <p>This application is designed to demonstrate a simple React setup with multiple pages.</p>
         </div>
     );
