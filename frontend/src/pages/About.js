88160fe6f9813e6155c1c40bc375587a2cd618e7
Melkzedk <melkywafula29@gmail.com>
2025-10-11 07:55:45 +0300

    Fix: include frontend folder

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,14 @@
+TextDecoderStreamtest
+TextDecoderStreamtest
+melk test
+// frontend/src/pages/About.js
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About This Application</h1>
+            <p>This application is designed to demonstrate a simple React setup with multiple pages.</p>
+        </div>
+    );
+}   
