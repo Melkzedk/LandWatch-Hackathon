786025f4d3626cfab36a03c2e381939776514f53
Melkzedk <melkywafula29@gmail.com>
2025-10-14 10:04:57 +0300

    changes

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -63,7 +63,7 @@ export default function Dashboard() {
 
   return (
     <div className="container mt-4">
-      <h3 className="mb-4 text-center">Land Analyses Dashboard</h3>
+      <h3 className="mb-4 text-center">Land Analyse Dashboard</h3>
 
       <Row>
         {/* Left Side - AI Form */}
@@ -172,7 +172,7 @@ export default function Dashboard() {
                 </tbody>
               </Table>
             </Card.Body>
-          </Card>
+          </Card> 
         </Col>
       </Row>
     </div>
