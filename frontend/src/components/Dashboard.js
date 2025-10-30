109e7396401a4b6e0cd1fdad5a757ef1defdcf76
Melkzedk <melkywafula29@gmail.com>
2025-10-30 13:23:40 +0300

    small changes

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -130,7 +130,7 @@ export default function Dashboard() {
                   <Button type="submit" variant="success" disabled={loading}>
                     {loading ? (
                       <>
-                        <Spinner animation="border" size="sm" /> Generating...Please wait
+                        <Spinner animation="border" size="sm" /> Generating..
                       </>
                     ) : (
                       "Generate AI Report"
