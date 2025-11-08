bb092d8624bd38d855197505bffaa1fa494b5459
Melkzedk <melkywafula29@gmail.com>
2025-11-08 13:51:17 +0300

    changes

--- frontend/src/test.js
+++
@@ -1,9 +0,0 @@
-import react from 'react';
-import App from './App';
-
-return App (
-    <div className="test-page">
-        <h1 className='head'>Test Page</h1>
-        <p>This is a test page for the frontend application.</p>
-    </div>
-);
