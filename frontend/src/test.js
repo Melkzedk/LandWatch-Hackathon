ce6fcf1040279390555ac6aab84a6a8022f8e0d2
Melkzedk <melkywafula29@gmail.com>
2025-11-08 13:51:08 +0300

    changes

--- frontend/src/test.js
+++ frontend/src/test.js
@@ -3,7 +3,7 @@ import App from './App';
 
 return App (
     <div className="test-page">
-        <h1>Test Page</h1>
+        <h1 className='head'>Test Page</h1>
         <p>This is a test page for the frontend application.</p>
     </div>
 );
